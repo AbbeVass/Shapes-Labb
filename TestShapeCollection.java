7c04f44ba5ace5d165f3b7705ae1c1ffdc9f49f0
ZataraE <erinderabigail@gmail.com>
2024-10-16 13:22:01 +0200

    La till tester som behövdes för ShapeCollection

--- TestShapeCollection.java
+++ TestShapeCollection.java
@@ -4,18 +4,39 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestShapeCollection {
     @Test
     public void testEmptyCollection() {
-        // TODO: Skriv klart tester för en tom ShapeCollection.
-        // Ta bort raden nedan innan du börjar.
-        // Kom ihåg Given, When, Then och se till att
-        // ALLA metoder i ShapeCollection täcks av dina tester.
-        assertTrue(false);
+        // Given
+        ShapeCollection ShapeCollection = new ShapeCollection();
+        // When
+        double totalArea = ShapeCollection.getTotalArea();
+        double totalNumberOfShapes = ShapeCollection.getNumberOfShapes();
+        // Then
+        // The number of shapes and the total area are both 0 since no shapes were added to the collection
+        int ExpectedNumberOfShapes = 0;
+        double ExpectedTotalArea = 0;
+        
+        assertEquals(ExpectedNumberOfShapes, totalNumberOfShapes, 0.001, "The total number of shapes shoulb be 0");
+        assertEquals(ExpectedTotalArea, totalArea, 0.001, "The total area should be 0");
     }
 
     @Test
     public void testSingleRectangle() {
-        // TODO: Skriv klart tester för en ShapeCollection med en rektangel.
-        // Ta bort raden nedan innan du börjar.
-        assertTrue(false);
+        // Given
+        ShapeCollection ShapeCollection = new ShapeCollection();
+        // When
+        ShapeCollection.addShape(new Rectangle(2.0,4.0));
+        double totalArea = ShapeCollection.getTotalArea();
+        double totalNumberOfShapes = ShapeCollection.getNumberOfShapes();
+        // Then
+        // Calculating the total area of the shapes in the collection
+        // The only shape in the collection is a Rectangle with: width = 2 and height = 4
+        // The rectangles area is 2*4 = 8
+        double RectangleArea = 2*4;
+        // The number of shapes are 1 since there is only one shape in the collection
+        int ExpectedNumberOfShapes = 1;
+        double ExpectedTotalArea = RectangleArea;
+
+        assertEquals(ExpectedNumberOfShapes, totalNumberOfShapes, 0.001, "The total number of shapes shoulb be 1");
+        assertEquals(ExpectedTotalArea, totalArea, 0.001, "The total area should be 2*4 = 8");
     }
 
     // TODO: Vilka fler tester behövs?
