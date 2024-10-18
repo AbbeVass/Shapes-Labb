91ab4a65b646b2b940f4b8bc31d40b87a92a5bfd
ZataraE <erinderabigail@gmail.com>
2024-10-18 10:49:45 +0200

    La till två nya tester, en som testar när den totala arean blir negativ och en som testar med flera former i TestShapeCollection.

--- TestShapeCollection.java
+++ TestShapeCollection.java
@@ -39,6 +39,57 @@ public class TestShapeCollection {
         assertEquals(ExpectedTotalArea, totalArea, 0.001, "The total area should be 2*4 = 8");
     }
 
-    // TODO: Vilka fler tester behövs?
-    // Lägg till minst två metoder som testar vanliga situationer med ShapeCollection.
+    @Test
+    public void testNegativeTotalArea() {
+        // Given
+        ShapeCollection ShapeCollection = new ShapeCollection();
+        // When
+        ShapeCollection.addShape(new Rectangle(-10.0,4.0));
+        ShapeCollection.addShape(new Circle(1.5));
+        ShapeCollection.addShape(new Square(2.0));
+        double totalArea = ShapeCollection.getTotalArea();
+        double totalNumberOfShapes = ShapeCollection.getNumberOfShapes();
+        // Then
+        // Calculating the total area of the shapes in the collection
+        // The only shape in the collection is a Rectangle with: width = 2 and height = 4
+        // The rectangles area is 10*4 = -40
+        double RectangleArea = -10*4;
+        // The Circles area is 1.5*1.5*PI = 7,0686...
+        double CircleArea = 1.5*1.5*Math.PI;
+        // The squares area is 2.0*2.0 = 4
+        double SquareArea = 2.0*2.0;
+        // The number of shapes are 1 since there is only one shape in the collection
+        int ExpectedNumberOfShapes = 3;
+        double ExpectedTotalArea = RectangleArea+CircleArea+SquareArea;
+
+        assertEquals(ExpectedNumberOfShapes, totalNumberOfShapes, 0.001, "The total number of shapes should be 3");
+        assertEquals(ExpectedTotalArea, totalArea, 0.001, "The total area should be -40+7,0686...+4 = -28,931... ");
+    }
+
+    @Test
+    public void testmultipleShapes() {
+        // Given
+        ShapeCollection ShapeCollection = new ShapeCollection();
+        // When
+        ShapeCollection.addShape(new Rectangle(10.0,4.0));
+        ShapeCollection.addShape(new Circle(1.5));
+        ShapeCollection.addShape(new Square(2.0));
+        double totalArea = ShapeCollection.getTotalArea();
+        double totalNumberOfShapes = ShapeCollection.getNumberOfShapes();
+        // Then
+        // Calculating the total area of the shapes in the collection
+        // The only shape in the collection is a Rectangle with: width = 2 and height = 4
+        // The rectangles area is 10*4 = 40
+        double RectangleArea = 10*4;
+        // The Circles area is 1.5*1.5*PI = 7,0686...
+        double CircleArea = 1.5*1.5*Math.PI;
+        // The squares area is 2.0*2.0 = 4
+        double SquareArea = 2.0*2.0;
+        // The number of shapes are 1 since there is only one shape in the collection
+        int ExpectedNumberOfShapes = 3;
+        double ExpectedTotalArea = RectangleArea+CircleArea+SquareArea;
+
+        assertEquals(ExpectedNumberOfShapes, totalNumberOfShapes, 0.001, "The total number of shapes should be 3");
+        assertEquals(ExpectedTotalArea, totalArea, 0.001, "The total area should be 40+7,0686...+4 = 51,0686... ");
+    }
 }
